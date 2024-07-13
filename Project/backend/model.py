46c82c1083b50ee0f94e3cb00279e16320d91ef1
Nishkarsh Sharma <nishkarshsharma540@gmail.com>
2024-07-13 19:45:55 +0530

    session2

---
+++ Project/backend/model.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from werkzeug.security import generate_password_hash
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grocery.db'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String,unique=True,nullable =False)
+    email = db.Column(db.String(50),nullable=False)
+    password = db.Column(db.String,nullable=False)
+    role = db.Column(db.String,default='user')
+
+
+    def __repr__(self):
+        return f'<User{self.username}>'
+    
+with app.app_context():
+    db.create_all()
+
+    if User.query.filter_by(username='admin').first() is None:
+        admin_password = generate_password_hash('adminpassword')
+
+        admin = User(username='admin',email='admin@grocery.com',password = admin_password,role='admin')
+
+        db.session.add(admin)
+        db.session.commit()
+    else:
+        print('Admin Already Exists')
